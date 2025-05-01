fbb7b2d472f5d5e169bb8cb4561308534aa41267
Samuel Njoroge <samuelnjoroge3455@gmail.com>
2025-05-01 12:21:41 +0300

    Enhance book routes with pagination and user-specific recommendations

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import { connectDB } from "./lib/db.js";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
-import bookRoutes from './routes/bookroutes.js';
-
+import bookRoutes from "./routes/bookRoutes.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 dotenv.config();
 const PORT = process.env.PORT || 5001;
 
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -36,8 +36,70 @@ router.post("/", protectRoute, async (req, res) => {
 
 router.get("/", protectRoute, async (req, res) => {
   try {
-    const books = await Book.find();
-    res.send(books);
+    // pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("username", "profileImage");
+    const totalBooks = await Book.countDocuments();
+    const totalPages = Math.ceil(totalBooks / limit);
+
+    res.status(200).json({
+      books,
+      currentPage: page,
+      totalPages,
+      totalBooks,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+//get recommended books when user is logged in
+router.get("/user", protectRoute, async (req, res) =>{
+  try {
+    const books = await Book.find({ user: req.user._id }).sort({ createdAt: -1 });
+    res.status(200).json(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+})
+
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    // Check if the user is the owner of the book
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to delete this book" });
+    }
+    // Delete the image from cloudinary
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.imageUrl.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.error("Error deleting image from Cloudinary:", deleteError);
+        return res
+          .status(500)
+          .json({ message: "Error deleting image from Cloudinary" });
+      }
+    }
+
+    await book.deleteOne();
+    res.status(200).json({ message: "Book deleted successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
